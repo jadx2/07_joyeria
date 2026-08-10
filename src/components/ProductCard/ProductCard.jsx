e6ddcfaa2ebfb650c55b308ac0fb774b523d2513
Jaim Delmar <jaimdelmar.dev@gmail.com>
2026-08-09 23:36:32 -0500

    feat(#54): componente ProductCard con precio formateado y Button reutilizado (#68)

---
+++ src/components/ProductCard/ProductCard.jsx
@@ -0,0 +1,21 @@
+import "./ProductCard.css"
+import Button from "../Button/Button"
+import { formatearPrecio } from "../../utils/moneda"
+
+function ProductCard({ imagen, nombre, material, precio, onAgregar }) {
+  return (
+    <div className="producto">
+      <img src={imagen} alt={nombre} className="producto__imagen" />
+      <h3 className="producto__nombre">{nombre}</h3>
+      <p className="producto__material">{material}</p>
+      <p className="producto__precio">{formatearPrecio(precio)}</p>
+      <div className="producto__accion">
+        <Button variant="primary" onClick={onAgregar}>
+          Add to Cart
+        </Button>
+      </div>
+    </div>
+  )
+}
+
+export default ProductCard
