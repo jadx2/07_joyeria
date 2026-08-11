5af5dcd2c2330cb463a7c4d3f0ea8c441a7d430d
Jaim Delmar <jaimdelmar.dev@gmail.com>
2026-08-11 11:45:41 -0500

    feat(#58): agrega la página 404 (#81)

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,22 @@
+import { Link } from "react-router-dom"
+import "./NotFound.css"
+import Container from "../components/Container/Container"
+import Button from "../components/Button/Button"
+
 const NotFound = () => {
-  return <h1>Page not found</h1>
+  return (
+    <section className="no-encontrado">
+      <Container>
+        <h1 className="no-encontrado__codigo">404</h1>
+        <p className="no-encontrado__texto">
+          The page you are looking for does not exist.
+        </p>
+        <Link to="/">
+          <Button variant="outline">Back to Home</Button>
+        </Link>
+      </Container>
+    </section>
+  )
 }
 
 export default NotFound
